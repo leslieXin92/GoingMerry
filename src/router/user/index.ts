1ad8d6fba8b47ae75c74f9a30df480025515782e
leslie <leslieXin92@gmail.com>
2023-07-30 17:33:16 +0800

    fix: file deps

--- src/router/user/index.ts
+++ src/router/user/index.ts
@@ -1,6 +1,6 @@
 import Router from 'koa-router'
 import { verifyLoginUserInfo, verifyRegisterUserInfo, encryptPassword } from '@/middleware'
-import { handleLogin, handleRegister, handleTest } from '@/controller'
+import { handleLogin, handleRegister } from '@/controller'
 import type { CustomContext } from '@/types'
 
 const userRouter: Router<any, CustomContext<any>> = new Router({ prefix: '/user' })
@@ -8,7 +8,5 @@ const userRouter: Router<any, CustomContext<any>> = new Router({ prefix: '/user'
 userRouter.post('/login', verifyLoginUserInfo, handleLogin)
 
 userRouter.post('/register', verifyRegisterUserInfo, encryptPassword, handleRegister)
-
-userRouter.get('/test', handleTest)
 
 export default userRouter
