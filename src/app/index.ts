2fb2573a7551b1b58dcb19d4d2143a2d1e4c7814
leslie <leslieXin92@gmail.com>
2023-10-26 01:33:42 +0800

    fix: koa2-cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,18 +1,18 @@
 import path from 'path'
 import Koa from 'koa'
 import bodyParse from 'koa-bodyparser'
+import cors from 'koa2-cors'
 import staticFiles from 'koa-static'
-import cors from '@koa/cors'
 import useRoutes from '@/router'
 import errorHandler from './errorHandler'
 
 const app = new Koa()
 
+app.use(cors())
+
 app.use(bodyParse())
 
 app.use(staticFiles(path.resolve(__dirname, '..', '../public')))
-
-app.use(cors())
 
 useRoutes(app)
 
