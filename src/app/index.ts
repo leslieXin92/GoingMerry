ebfe3a599759855e74b64148f37a8734e143a394
leslie <leslieXin92@gmail.com>
2023-10-26 01:01:10 +0800

    chore: del cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,11 @@
 import path from 'path'
 import Koa from 'koa'
 import bodyParse from 'koa-bodyparser'
-import cors from 'koa2-cors'
 import staticFiles from 'koa-static'
 import useRoutes from '@/router'
 import errorHandler from './errorHandler'
 
 const app = new Koa()
-
-app.use(cors())
 
 app.use(bodyParse())
 
