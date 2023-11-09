9b08be43ba067259b02bd671124ba7b7b0416fdd
leslie <leslieXin92@gmail.com>
2023-11-09 23:32:51 +0800

    update: build

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,9 +1,13 @@
 const { DefinePlugin } = require('webpack')
-const nodeExternals = require('webpack-node-externals')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { resolve } = require('path')
+const nodeExternals = require('webpack-node-externals')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   resolve: {
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
     extensions: ['.ts', '.js']
   },
   target: 'node',
@@ -11,30 +15,31 @@ module.exports = {
     server: `${process.cwd()}/src/main.ts`
   },
   output: {
-    filename: 'main.js',
-    path: `${process.cwd()}/dist`
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader'
+        use: 'ts-loader'
       }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(['production', 'prod'].includes(process.env.NODE_ENV) ? 'production' : 'development')
       }
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/app/keys/private.key', to: 'keys/private.key' },
+        { from: 'src/app/keys/public.key', to: 'keys/public.key' }
+      ]
+    }),
+    new CleanWebpackPlugin()
   ],
   externals: [nodeExternals()]
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,10 @@
 const { merge } = require('webpack-merge')
-const TersetWebpackPlugin = require('terser-webpack-plugin')
 const baseWebpackConfig = require('./webpack.config.base')
 
 const webpackConfig = merge(baseWebpackConfig, {
   devtool: 'eval-cheap-source-map',
   mode: 'production',
-  stats: { children: false },
-  optimization: {
-    minimize: true,
-    minimizer: [new TersetWebpackPlugin()],
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          name: 'commons',
-          chunks: 'initial',
-          minChunks: 3,
-          enforce: true
-        }
-      }
-    }
-  }
+  stats: { children: false }
 })
 
 module.exports = webpackConfig
